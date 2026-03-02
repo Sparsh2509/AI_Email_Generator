005d8c5248467bd8b56b8d3dc866a1c6296c95e1
Sparsh2509 <sparshgupta250904@gmail.com>
2026-03-03 00:00:33 +0530

    Added api key in env

--- rag/build_vectorstore.py
+++ rag/build_vectorstore.py
@@ -3,6 +3,10 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def build_vectorstore():
     docs = []
@@ -20,7 +24,8 @@ def build_vectorstore():
     split_docs = splitter.split_documents(docs)
 
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001"
+        model="models/embedding-001",
+        google_api_key=os.getenv("GOOGLE_API_KEY")
     )
 
     vectorstore = FAISS.from_documents(split_docs, embeddings)
