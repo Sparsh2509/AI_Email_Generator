db6243857c347a23a299a8fbb70974d8695a2fdc
Sparsh2509 <sparshgupta250904@gmail.com>
2026-03-03 00:15:33 +0530

    Using Hugging face embeddings now

--- rag/build_vectorstore.py
+++ rag/build_vectorstore.py
@@ -1,21 +1,30 @@
 import os
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
-from langchain_community.vectorstores import FAISS
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
-import os
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 load_dotenv()
 
 def build_vectorstore():
-    docs = []
 
-    for file in os.listdir("Email_Templates_idea"):
-        if file.endswith(".txt"):
-            loader = TextLoader(f"Email_Templates_idea/{file}")
-            docs.extend(loader.load())
+   
+    loader = DirectoryLoader(
+        "Email_Templates_idea",
+        glob="**/*.txt",
+        loader_cls=TextLoader
+    )
 
+    docs = loader.load()
+    print(f"Loaded {len(docs)} documents")
+
+    
+    for doc in docs:
+        filename = os.path.basename(doc.metadata["source"])
+        doc.metadata["filename"] = filename
+
+    
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
         chunk_overlap=50
@@ -23,13 +32,17 @@ def build_vectorstore():
 
     split_docs = splitter.split_documents(docs)
 
-    embeddings = GoogleGenerativeAIEmbeddings(
-        model="text-embedding-004"
-        google_api_key=os.getenv("GEMINI_API_KEY")
+    
+    embeddings = HuggingFaceEmbeddings(
+        model_name="all-MiniLM-L6-v2"
     )
 
+    
     vectorstore = FAISS.from_documents(split_docs, embeddings)
+
     vectorstore.save_local("faiss_index")
+
+    print("Vectorstore built successfully using HuggingFace embeddings!")
 
 if __name__ == "__main__":
     build_vectorstore()
